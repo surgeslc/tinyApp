222d603ca795eb42c42fa4f57b09001cac607abf
Lawrence Surges <surgeslc@gmail.com>
2017-07-06 00:55:06 +0000

    Part way through Activity 171

--- express_server.js
+++ express_server.js
@@ -2,12 +2,28 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs")
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  var string_length = 6;
+  var randomstring = '';
+  for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum,rnum+1);
+  }
+  console.log(randomstring);
+  return randomstring;
+}
+//generateRandomString();
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -19,8 +35,30 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// Must be read before "urls/:id"
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, URL: urlDatabase[req.params.id] };
+  // console.log("urls/:id:", templateVars);
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  //const URL = req.body;
+  //console.log(req.body);
+  console.log("Short:",shortURL);  // debug statement to see POST parameters
+  console.log("Long:", req.body.longURL);
+  urlDatabase[shortURL] = req.body["longURL"];
+  console.log(urlDatabase);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
