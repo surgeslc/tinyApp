7afac75fbbfc8be7a0c88dfeda67383737293aff
Lawrence Surges <surgeslc@gmail.com>
2017-07-25 09:41:39 +0000

    Progress on encryption, cookie sessions

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
+// Requirements & Uses
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
+
+const bcrypt = require('bcrypt');
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -8,11 +12,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  userId: 'session',
+  keys: ['freddy', 'frog']
+}));
+
 //const ejsLint = require('ejs-lint');
 //app.use(ejslint());
 
 app.set("view engine", "ejs")
 
+//
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -20,16 +31,6 @@ var urlDatabase = {
 //console.log(urlDatabase);
 
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  },
   "user3RandomID": {
     id: "user3RandomID",
     email: "hilary@hilarymackey.ca",
@@ -38,11 +39,30 @@ const users = {
   "user4RandomID": {
     id: "user4RandomID",
     email: "lawrence@lawrencesurges.ca",
-    password: "funny-bunny"
+    password: bcrypt.hashSync("funny-bunny", 10)
   }
 };
 
+
 /* HELPER FUNCTION(S) */
+
+const checkPassword = (emailIn, passwordIn) => {
+  if (bcrypt.compareSync(passwordIn, users[getIdByEmail(emailIn)].password)) {
+    return true;
+  }
+  return false;
+}
+
+const doesEmailExist = (emailIn) => {
+  for(user in users) {
+    if (users[user].email === emailIn) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Inconsistent capitalization - can't correct here alone without consequences
 function generateRandomString() {
   const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
   const string_length = 6;
@@ -54,6 +74,24 @@ function generateRandomString() {
   return randomstring;
 }
 
+const getEmailById = (userID) => {
+  let user = users[userID];
+  if (!users) {
+    return;
+  }
+  return user.email;
+};
+
+const getIdByEmail = (emailIn) => {
+  let id = undefined;
+  for(user in users) {
+    if (users[user].email === emailIn) {
+      id = user;
+    }
+  }
+  return id;
+};
+
 /* ROUTES */
 /* gets */
 app.get("/", (req, res) => {
@@ -61,8 +99,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
-  res.render("register");
+  if (req.session.userId) {
+    res.redirect("/urls");
+    return;
+  } else {
+    let userEmail = getEmailById(req.session.userId);
+    let templateVars = { userEmail: userEmail };
+    res.render("register", templateVars);
+  }
 });
 
 app.get("/urls", (req, res) => {
@@ -114,9 +158,19 @@ app.post("/register", (req, res) => {
   let userID = generateRandomString();
   let email = req.body.email;
   let password = req.body.password;
-  users[userID] = {id: userID, email: email, password: password};
-  let templateVars = {username: req.cookies["username"] };
-  res.redirect("/urls");
+  if ((email = "") || (password = "")) {
+    alert("Email address and password are required");
+    res.render("/register");
+  } else if (Object.values(users).indexOf(req.body.email) > -1) {
+    // Email address is already in users
+    alert("Sorry,", email, "was registered by another user");
+    res.render("/register");
+  } else {
+    users[userID] = {id: userID, email: email, password: password};
+    let templateVars = {username: req.cookies["username"] };
+    console.log("templateVars:", templateVars);
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls", (req, res) => {
